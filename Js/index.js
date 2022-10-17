02db4edb7bf7fc38258fa977d07c9c14b85323c8
KosinKosinski <kosinkosinski@gmail.com>
2022-10-17 20:49:32 +0200

    Create index.js

---
+++ Js/index.js
@@ -0,0 +1,9 @@
+<div id="twitch-embed"></div>
+
+<script src="https://player.twitch.tv/js/embed/v1.js"></script>
+
+<script type="text/javascript">
+  new Twitch.Player("twitch-embed", {
+    channel: "kosinkosinski"
+  });
+</script>
