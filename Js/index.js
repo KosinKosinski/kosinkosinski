f7b5bd29d2eb623bed52c96f39906b9a435a3136
KosinKosinski <kosinkosinski@gmail.com>
2022-10-17 22:37:05 +0200

    q

--- Js/index.js
+++
@@ -1,9 +0,0 @@
-<div id="twitch-embed"></div>
-
-<script src="https://player.twitch.tv/js/embed/v1.js"></script>
-
-<script type="text/javascript">
-  new Twitch.Player("twitch-embed", {
-    channel: "kosinkosinski"
-  });
-</script>
